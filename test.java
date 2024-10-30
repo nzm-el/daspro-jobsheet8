176187ba59115f22c3dd4abaadb7ac7380161b41
nzm-el <nizamel1402@gmail.com>
2024-10-30 08:44:11 +0700

    pertemuan ke 8

---
+++ test.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+public class test {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int i, j;
+        float nilai, totalNilai, rataNilai;
+
+        i = 1;
+            while (i <= 5) {
+                totalNilai = 0;
+                System.out.println("Input Nilai Mahasiswa ke- " + i);
+                for (j = 1; j <= 5; j++) {
+                    System.out.print("Nilai ke-" + j + " = ");
+                    nilai = sc.nextInt();
+                    totalNilai += nilai;
+                }
+            rataNilai = totalNilai/5;
+            System.out.println("Rata-rata Nilai Mahasiswa ke- " + i + "=" + rataNilai);
+            i++;
+            }
+        }
+    }
+
